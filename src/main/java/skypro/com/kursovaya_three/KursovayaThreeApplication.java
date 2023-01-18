998f9f83e25b785d97e861ec6b8c488a46789803
Scherbakova <nastyasch0806@gmail.com>
2023-01-18 15:34:59 +0300

    first commit

---
+++ src/main/java/skypro/com/kursovaya_three/KursovayaThreeApplication.java
@@ -0,0 +1,13 @@
+package skypro.com.kursovaya_three;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursovayaThreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursovayaThreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/com/kursovaya_three/KursovayaThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.com.kursovaya_three;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursovayaThreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
